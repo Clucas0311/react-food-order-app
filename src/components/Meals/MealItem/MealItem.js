7de808a8f55eb181167ac912226eea19ce081474
Clucas0311 <clucas0311@gmail.com>
2022-02-17 16:07:20 -0500

    Added Functionality To Add items to cart

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import MealItemForm from "./MealItemForm";
 import classes from "./MealItem.module.css";
+import CartContext from "../../../store/cart-context";
 
 const MealItem = ({ id, name, description, price }) => {
+  const cartCtx = useContext(CartContext);
   const newPrice = `$${price.toFixed(2)}`;
+
+  const addToCartHandler = (amount) => {
+    cartCtx.addItem({
+      id: id,
+      name: name,
+      amount: amount,
+      price: price,
+    });
+  };
   return (
     <li className={classes.meal}>
       <div>
@@ -13,7 +24,7 @@ const MealItem = ({ id, name, description, price }) => {
         <div className={classes.price}>{newPrice}</div>
       </div>
       <div>
-        <MealItemForm id={id} />
+        <MealItemForm id={id} onAddToCart={addToCartHandler} />
       </div>
     </li>
   );
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,12 +1,33 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 import Input from "../../UI/Input";
 import classes from "./MealItemForm.module.css";
 
-const MealItemForm = ({ id }) => {
+const MealItemForm = ({ id, onAddToCart }) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim() === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    onAddToCart(enteredAmountNumber);
+  };
   return (
-    <form className={classes.form} action="">
+    <form className={classes.form} onSubmit={submitHandler} action="">
       <Input
+        ref={amountInputRef}
         label="Amount"
         input={{
           id: "amount" + id,
@@ -19,6 +40,7 @@ const MealItemForm = ({ id }) => {
         type="text"
       />
       <button type="submit">+ Add</button>
+      {!amountIsValid && <p>Please Enter a valid amount (1-5)</p>}
     </form>
   );
 };
